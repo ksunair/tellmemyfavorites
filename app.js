337c126b19c973e7f82bde644246e785a44abe38
ksunair <ksunair@yahoo.com>
2016-01-11 22:43:02 -0600

    Added example of display hello end point

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var express = require('express');
+var express = require('express'),
+    helloController = require('./controllers/helloHelper');
 
 var port = process.env.PORT || 8000;
 
@@ -12,11 +13,16 @@ app.get('/', function (req, res) {
     res.send('Welcome to Best API unni can write');
 });
 
+router.route('/hello')
+    .get(helloController.displayHello)
+
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
+
+
 
 
 app.listen(port, function () {
---
+++ controllers/helloHelper.js
@@ -0,0 +1,3 @@
+exports.displayHello = function (req, res) {
+    res.send("Hello");
+};
