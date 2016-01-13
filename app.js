bdc5e93914a366ebf3e2e53a5d12ccc68c574f5c
ksunair <ksunair@yahoo.com>
2016-01-12 22:04:05 -0600

    added cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    cors = require('cors'),
     helloController = require('./controllers/helloHelper'),
     usersController = require('./controllers/userHelper');
 
@@ -8,6 +9,7 @@ var router = express.Router();
 
 var app = express();
 
+app.use(cors())
 app.use('/api', router);
 
 app.get('/', function (req, res) {
